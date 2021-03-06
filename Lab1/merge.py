d4c5d5a0d01379170a7cdc510c1bf4dca6093046
Roland Bauer <wi19b066@technikum-wien.at>
2021-03-06 08:39:46 +0100

    already workin', but slow
    nimmt aktuell nur die ersten 4 Filme aus jedem File; funktioniert; ist aber m.E. doch sehr langsam (zumindest in meiner VM); ist die Handschrift eines Python-Anfängers ;)

--- Lab1/merge.py
+++ Lab1/merge.py
@@ -1,7 +1,58 @@
 import sys
+import os
+import csv
 
-inputDir = sys.argv[1]
-outputFile = sys.argv[2]
 
-print(inputDir)
-print(outputFile)
+def get_file_name(input_dir, i):
+    return input_dir + "combined_data_" + str(i) + ".txt"
+
+
+# say hello
+print("\n*******************************")
+print("Hello to kappa's merger!")
+print("*******************************\n")
+
+# read arguments, set input directory and output file
+argList = list(sys.argv)
+if len(argList) > 1:
+    inputDir = argList[1]
+    if not (os.path.isdir(inputDir)):
+        exit()
+    outputFile = argList[2]
+    print("Dir and File argument ok!\n")
+else:
+    inputDir = ""
+    outputFile = "_.csv"
+    print("Standard Dir and File set!\n")
+
+for i in [1, 2, 3, 4]:
+    filepath = get_file_name(inputDir, i)
+    if not os.path.isfile(filepath):
+        print("At least one file missing! Program stops!")
+        exit()
+    else:
+        print(filepath + " found")
+
+print("\nStart creating CSV...\n")
+with open(outputFile, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for filenumber in [1, 2, 3, 4]:
+        filepath = get_file_name(inputDir, i)
+        with open(filepath, "r") as file:
+            for line in file:
+                if line.__contains__("5:"):
+                    break
+                line = line.rstrip()
+                if line.__contains__(":"):
+                    movie_id = line.replace(":", "")
+                    continue
+                line = line.split(",")
+                writer.writerow([int(movie_id)] + [line[0]] + [line[1]] + [line[2]])
+        print(filepath + " done.")
+
+
+# say bye
+print("\n*******************************")
+print("Finished merge. CSV saved to " + outputFile + ". See you!")
+print("*******************************\n")
