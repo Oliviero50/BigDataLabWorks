4caa73a115dba14186bde2fe7cc15bd3dd8da03d
mbpf1090 <sebastian.weidele@gmail.com>
2021-04-25 07:28:46 +0200

    pyspark sends msg back to kafka

--- Lab6/kafka/consumer.py
+++ Lab6/kafka/consumer.py
@@ -4,7 +4,7 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
   auto_offset_reset='earliest')
 #consumer.subscribe(['testtopic'])
-consumer.subscribe(['streams-linesplit-output'])
+consumer.subscribe(['pyspark'])
 
 for msg in consumer:
   print (msg)
--- Lab6/spark.py
+++ Lab6/spark.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
+from pyspark.sql.functions import current_timestamp
+
 
 spark = SparkSession \
     .builder \
@@ -19,12 +21,12 @@ df = spark \
 df.printSchema()
 df.selectExpr("CAST(value AS STRING)")
 
-# Split the lines into words
+# Split the lines into words and add timestamp column
 words = df.select(
    explode(
        split(df.value, " ")
    ).alias("word")
-)
+).withColumn("timestamp", current_timestamp())
 
 # Generate running word count
 wordCounts = words.groupBy("word").count()
@@ -34,5 +36,15 @@ query = wordCounts \
     .outputMode("complete") \
     .format("console") \
     .start()
+
+# Send words back to Kafka
+# query = words \
+#     .selectExpr("CAST(timestamp AS STRING) AS key", "CAST(word AS STRING) AS value") \
+#     .writeStream \
+#     .format("kafka") \
+#     .option("checkpointLocation", "./checkpoint") \
+#     .option("kafka.bootstrap.servers", "localhost:9092") \
+#     .option("topic", "pyspark") \
+#     .start() 
 
 query.awaitTermination()
