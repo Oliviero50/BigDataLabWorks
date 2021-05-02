fb3cd1818a1dead9070fbf2a1d08bdb44f8e0459
roland6bauer <ola1@gmx.com>
2021-05-02 17:47:42 +0200

    working spark.py with stream back to kafka

--- Lab6/spark/spark.py
+++ Lab6/spark/spark.py
@@ -1,50 +1,57 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 from pyspark.sql.functions import current_timestamp
 
-
 spark = SparkSession \
     .builder \
-    .appName("StructuredNetworkWordCount") \
+    .appName("StructuredNetworkHighwayAnalysis") \
     .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+# Define schema of json
+schema = StructType() \
+    .add("type", StringType()) \
+    .add("highway", StringType()) \
+    .add("timestamp", TimestampType())
 
 # Create DataFrame representing the stream of input lines from connection to localhost:9999
 df = spark \
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "testtopic") \
+    .option("subscribe", "cartopic") \
     .load()
 
 df.printSchema()
-df.selectExpr("CAST(value AS STRING)")
+tmp = df.selectExpr("CAST(value AS STRING)")
+cardata = tmp.select(
+    from_json(col("value"), schema).alias("data")).select("data.*")
+cardata.printSchema()
 
-# Split the lines into words and add timestamp column
-words = df.select(
-   explode(
-       split(df.value, " ")
-   ).alias("word")
-).withColumn("timestamp", current_timestamp())
+windowedCounts = cardata \
+    .groupBy(
+        window(cardata.timestamp, "10 seconds", "10 seconds"),
+        cardata.type
+    ).count()
+windowedCounts.printSchema()
 
-# Generate running word count
-wordCounts = words.groupBy("word").count()
+# query = windowedCounts.writeStream \
+#     .outputMode("complete") \
+#     .format("console") \
+#     .option('truncate', 'false') \
+#     .start()
 
-query = wordCounts \
+query = windowedCounts \
+    .selectExpr("to_json(struct(*)) AS value") \
     .writeStream \
+    .format("kafka") \
     .outputMode("complete") \
-    .format("console") \
+    .option("checkpointLocation", "./checkpoint") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("topic", "pyspark") \
     .start()
-
-# Send words back to Kafka
-# query = words \
-#     .selectExpr("CAST(timestamp AS STRING) AS key", "CAST(word AS STRING) AS value") \
-#     .writeStream \
-#     .format("kafka") \
-#     .option("checkpointLocation", "./checkpoint") \
-#     .option("kafka.bootstrap.servers", "localhost:9092") \
-#     .option("topic", "pyspark") \
-#     .start() 
 
 query.awaitTermination()
